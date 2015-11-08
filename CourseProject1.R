60ba9c53244211969927ea0a09b82c1783edca72
Nature Song <sol2man2@gmail.com>
2015-11-08 23:53:15 +0900

    CourseProject1.R 추가

---
+++ CourseProject1.R
@@ -0,0 +1,12 @@
+
+hpc <- read.table("household_power_consumption.txt", sep=";", header=T)
+
+dim(hpc)
+colnames(hpc)
+head(hpc,1)
+
+head(hpc[,1])
+class(hpc[,1])
+
+filtered.index <- which(hpc$Date == "1/2/2007" | hpc$Date == "2/2/2007")
+filtered.hpc <- hpc[filtered.index,]
